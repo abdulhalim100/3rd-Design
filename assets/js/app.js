99a272edcd1f9728961fcc64f6f491c1f71d27b7
abdulhalim100 <abdulhalim500083@gmail.com>
2024-10-17 00:06:56 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+    var swiper = new Swiper(".heroSlider", {
+      
+      loop:true,
+      autoplay:true,
+        cssMode: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+    var swiper = new Swiper(".carSlider", {
+    
+      loop:true,
+      autoplay:true,
+        cssMode: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+      /*
+      breakpoints: {
+        440: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 50,
+        },
+      },
+        
+    });*/
