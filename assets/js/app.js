fa932acda3767752c33f3a394bc9f6a3b73617cf
abdulhalim100 <abdulhalim500083@gmail.com>
2025-08-08 15:05:38 +0600

    Brand Section Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+/*----------------------------------Hero Slider Start-----------------*/
+
 var swiper = new Swiper(".heroSlider", {
   loop: true,
   spaceBetween: 30,
@@ -14,44 +16,114 @@ var swiper = new Swiper(".heroSlider", {
   },
   pagination: {
     el: ".swiper-pagination",
+    speed: 1000,
+    effect: "fade",
+    clickable: true,
   },
   mousewheel: true,
   keyboard: true,
 });
-
-var swiper = new Swiper(".mySwiper", {
+/*----------------------------------Hero Slider Close-----------------*/
+/*----------------------------------Brand Slider Start-----------------*/
+var swiper = new Swiper(".myCustomSwiper", {
   loop: true,
   spaceBetween: 10,
-  slidesPerView: 4,
+  slidesPerView: 7,
   freeMode: true,
+  speed: 1000,
   watchSlidesProgress: true,
 });
 var swiper2 = new Swiper(".mySwiper2", {
   loop: true,
+  speed: 2000,
+
+  // effect Start------------------------------------------------------
+
+  effect: "cube",
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  effect: "flip",
+  flipEffect: {
+    slideShadows: true,
+    limitRotation: true,
+  },
+
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+
+  effect: "cards",
+  grabCursor: true,
+
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+
+  // effect Close------------------------------------------------------
+
   spaceBetween: 10,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".car-swiper-button-next",
+    prevEl: ".car-swiper-button-prev",
   },
   thumbs: {
     swiper: swiper,
   },
 });
+/*----------------------------------Brand Slider Close-----------------*/
+/*----------------------------------Car Studio Slider Start-----------------*/
 
 var swiper = new Swiper(".carSlider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
   loop: true,
-  autoplay: true,
-  cssMode: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
   mousewheel: true,
   keyboard: true,
 });
+/*----------------------------------Car Studio Slider Close-----------------*/
+
 /*
       breakpoints: {
         440: {
